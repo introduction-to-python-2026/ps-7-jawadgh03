539c0944a579a79818b1523f3320a42731eff2d9
jawadgh03 <jawadghaname12@gmail.com>
2025-12-16 15:04:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+
+iris = load_iris(as_frame=True)
+df = iris.frame
+
+x = df['sepal length (cm)']
+y = df['petal length (cm)']
+plt.scatter(x, y)
+plt.xlabel('Sepal Length')
+plt.ylabel('Petal Length')
+plt.title('Simple Correlation Plot')
+
+plt.savefig('correlation.png')
+plt.show()
 
